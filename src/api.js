33d2aff78bdd60621f75047e160f1ee45dc5f19d
周源培 <1181588051@qq.com>
2021-11-16 19:09:00 +0800

    feat: 完成文章管理部分代码

--- src/api.js
+++ src/api.js
@@ -2,98 +2,100 @@
 import axios from 'axios'
 
 // const api_host_local = 'http://127.0.0.1/book_spider/public/'
-const api_host_company = 'http://192.168.6.98/book_spider/public/'
+// const api_host_company = 'http://192.168.6.98/book_spider/public/'
+const api_host = 'http://23.105.214.24/blog/'
 
 
-function httpRequest(url,method = 'get',params,charset,callback){
+
+function httpRequest(url, method = 'get', params, charset, callback) {
   var apiUrl = ''
-  if(url.indexOf('http') !== -1){
+  if (url.indexOf('http') !== -1) {
     apiUrl = url
-  }else{
-    apiUrl = api_host_company + url
+  } else {
+    apiUrl = api_host + url
   }
-  if(method === 'get'){
-      axios.get(apiUrl, {
-        params: params
-      })
+  if (method === 'get') {
+    axios.get(apiUrl, {
+      params: params
+    })
       .then(function (response) {
         callback(response.data.data)
       })
       .catch(function (error) {
         console.log(error);
       });
-  }else{
+  } else {
     axios.post(apiUrl, params)
-    .then(function (response) {
-      callback(response.data)
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+      .then(function (response) {
+        callback(response.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
 }
 
 
-export function getArticleList(url,method = "get",params,callback,charset="utf-8"){
-  return httpRequest(url,method,params,charset,callback)
+export function getArticleList(params, callback, charset = "utf-8") {
+  return httpRequest('articles', 'get', params, charset, callback)
 }
 
-export function getWordList(url,method = "get",params,callback,charset="utf-8"){
-  return httpRequest(url,method,params,charset,callback)
+export function getWordList(url, method = "get", params, callback, charset = "utf-8") {
+  return httpRequest(url, method, params, charset, callback)
 }
 
-export function wordUpdate(url,method = "post",params,callback,charset="utf-8"){
-  return httpRequest(url,method,params,charset,callback)
+export function wordUpdate(url, method = "post", params, callback, charset = "utf-8") {
+  return httpRequest(url, method, params, charset, callback)
 }
 
 /**处理章节数据 */
-export function handleChapter(url,method = "post",params,callback,charset="utf-8"){
-  return httpRequest(url,method,params,charset,callback)
+export function handleChapter(url, method = "post", params, callback, charset = "utf-8") {
+  return httpRequest(url, method, params, charset, callback)
 }
 
 /**处理书籍数据 */
-export function handleBook(url,method = "post",params,callback,charset="utf-8"){
-  return httpRequest(url,method,params,charset,callback)
+export function handleBook(url, method = "post", params, callback, charset = "utf-8") {
+  return httpRequest(url, method, params, charset, callback)
 }
 
 /**处理书籍数据 */
-export function handleBookExport(url,method = "get",params,callback,charset="utf-8"){
-  return httpRequest(url,method,params,charset,callback)
+export function handleBookExport(url, method = "get", params, callback, charset = "utf-8") {
+  return httpRequest(url, method, params, charset, callback)
 }
 
 /**章节数据爬取 */
-export function handleChapterSpider(url,method = "get",params,callback,charset="utf-8"){
-  return httpRequest(url,method,params,charset,callback)
+export function handleChapterSpider(url, method = "get", params, callback, charset = "utf-8") {
+  return httpRequest(url, method, params, charset, callback)
 }
 
 /**获取直播推流 */
-export function getVideoList(params,callback,charset="utf-8"){
-  return httpRequest('http://192.168.6.19/video/Video.php','get',params,charset,callback)
+export function getVideoList(params, callback, charset = "utf-8") {
+  return httpRequest('http://192.168.6.19/video/Video.php', 'get', params, charset, callback)
 }
 
 
 /**获取套图列表 */
-export function getImgsList(url,method = "get",params,callback,charset="utf-8"){
-  return httpRequest(url,method,params,charset,callback)
+export function getImgsList(url, method = "get", params, callback, charset = "utf-8") {
+  return httpRequest(url, method, params, charset, callback)
 }
 
 /**获取套图详情 */
-export function getImgsInfo(url,method = "get",params,callback,charset="utf-8"){
-  return httpRequest(url,method,params,charset,callback)
+export function getImgsInfo(url, method = "get", params, callback, charset = "utf-8") {
+  return httpRequest(url, method, params, charset, callback)
 }
 
 
 /**获取网站列表 */
-export function getWebsiteList(url,method = "get",params,callback,charset="utf-8"){
-  return httpRequest(url,method,params,charset,callback)
+export function getWebsiteList(url, method = "get", params, callback, charset = "utf-8") {
+  return httpRequest(url, method, params, charset, callback)
 }
 
 /**获取书籍列表 */
-export function getBookList(params,callback){
-  return httpRequest('admin/book/list','get',params,'utf-8',callback)
+export function getBookList(params, callback) {
+  return httpRequest('admin/book/list', 'get', params, 'utf-8', callback)
 }
 
-export function getSpiderCommond(params,callback){
-  return httpRequest('admin/book/getSpiderCommond','get',params,'utf-8',callback)
+export function getSpiderCommond(params, callback) {
+  return httpRequest('admin/book/getSpiderCommond', 'get', params, 'utf-8', callback)
 }
--- src/components/pages/Article/index.jsx
+++ src/components/pages/Article/index.jsx
@@ -3,94 +3,121 @@ import { Table } from 'antd';
 import BreadcrumbComponent from '../../Breadcrumb/index'
 import ActionButton from '../../ActionButton/index'
 
-
-
-
-const data = [
-  {
-    key: '1',
-    id: '1',
-    name: 'John Brown',
-    money: '￥300,000.00',
-    address: 'New York No. 1 Lake Park',
-  },
-  // {
-  //   key: '2',
-  //   id: '2',
-  //   name: 'Jim Green',
-  //   money: '￥1,256,000.00',
-  //   address: 'London No. 1 Lake Park',
-  // },
-  // {
-  //   key: '3',
-  //   id: '3',
-  //   name: 'Joe Black',
-  //   money: '￥120,000.00',
-  //   address: 'Sidney No. 1 Lake Park',
-  // },
-];
-
-
-
+import { getArticleList } from '../../../api';
 export default class ArticleList extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      tableData: [],
+      paginationProps: {
+        position: ['none', 'bottomRight'],
+        showSizeChanger: false,
+        showQuickJumper: false,
+        showTotal: () => `共0条`,
+        pageSize: 10,
+        current: this.props.match.params.page ? this.props.match.params.page : 1,
+        total: 0,
+        // onChange: (current) => this.changePage(current),
+      },
+      // searchData: {
+      //   book_name: '测试',
+      //   author_name: ''
+      // },
+      // searchLocalStorageKey: 'book_list_search_data',
+    };
+
     this.columns = [
       {
         title: '文章标题',
-        dataIndex: 'name',
+        dataIndex: 'title',
         width: 200,
-        align:'center',
-      },
-      {
-        title: 'Cash Assets',
-        className: 'column-money',
-        dataIndex: 'money',
         align: 'center',
       },
       {
-        title: 'Address',
-        dataIndex: 'address',
+        title: '封面',
+        dataIndex: 'cover',
+        align: 'center',
+        // < a target = "_blank" rel = "noopener noreferrer" href = { '/book/info/' + record.key } > { text }</a >
+        render: (text, record) => <img style={{ height: '80px' }} src={record.cover} />,
+      },
+      {
+        title: '详情',
+        dataIndex: 'content',
       },
       {
         title: '操作',
         dataIndex: 'address',
         align: 'center',
-        render: (text, record, index) => <ActionButton editClick={this.handleEdit} deleteClick={this.handleDelete} value={record}/>
+        render: (text, record, index) => <ActionButton editClick={this.handleEdit} deleteClick={this.handleDelete} value={record} />
       },
     ];
-    
+
     this.loadData = this.loadData.bind(this);
     this.handleEdit = this.handleEdit.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
   }
+  componentDidMount() {
+    this.loadData()
+  }
   /*加载数据*/
-  loadData(){
+  loadData(isInit = true) {
 
+    const _this = this
+
+    const params = {
+      page: 1,
+      limit: 10
+    }
+
+    getArticleList(params, function (res) {
+      const data = res.data.map((item, index) => {
+        var temp = []
+        temp['key'] = item.id
+        temp['title'] = item.title
+        temp['cover'] = item.cover
+        temp['content'] = item.content
+        temp['created_at'] = item.created_at
+        temp['current_page'] = item.current_page
+        return temp
+      })
+
+      /**更新分页信息 */
+      const pageData = _this.state.paginationProps
+      pageData.total = res.total
+      pageData.current = res.current_page
+      pageData.pageSize = res.per_page
+      pageData.showTotal = () => `共` + res.total + `条`
+
+      _this.setState({
+        tableData: data,
+        paginationProps: pageData
+      });
+    })
   }
   /*编辑数据*/
-  handleEdit(id){
+  handleEdit(id) {
     alert(id)
   }
   /*删除数据*/
-  handleDelete(id){
+  handleDelete(id) {
     alert(id)
   }
-  handleClick(){
+  handleClick() {
     alert('hahah');
   }
   render() {
     return (
       <>
-        <BreadcrumbComponent path='文章管理/文章列表'/>
-        
+        <BreadcrumbComponent path='文章管理/文章列表' />
+
         <Table
           columns={this.columns}
-          dataSource={data}
+          dataSource={this.state.tableData}
+          pagination={this.state.paginationProps}
           bordered
-          // title={() => 'Header'}
-          // footer={() => 'Footer'}
+        // title={() => 'Header'}
+        // footer={() => 'Footer'}
         />
       </>
     );
